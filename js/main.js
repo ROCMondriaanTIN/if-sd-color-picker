3a01c3a59d890e1c54109401272360898e7782a2
Matthijs <>
2023-04-12 10:02:48 +0200

    Done for now

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 console.log("main.js loaded");
 
+const someNumber = 255;
+console.log(someNumber + " in hex is " + someNumber.toString(16));
+
+
 // Variables
 const bgColor = {
     r: 12, 
@@ -8,45 +12,54 @@ const bgColor = {
 
     toCSS: function () {
         let result = `rgb(${this.r},${this.g}, ${this.b})`;
+        console.log("Color in RGB is: ") + result;
 
         return result;
     },
 
     toHex: function () {
         let result = "#";
-        // bewerk hier het resultaat naar een hexadecimaal getal. 
+        // Bewerk hier het resultaat naar een hexadecimaal getal. 
         result += Number(this.r).toString(16);
         result += Number(this.g).toString(16);
         result += Number(this.b).toString(16);
+        console.log("Color in hexadecimals is: " + result);
+
         return result;
     }
 }
+
 
 const redSlider = document.querySelector("input.red");
 const greenSlider = document.querySelector("input.green");
 const blueSlider = document.querySelector("input.blue");
 
+
 //Event listeners
 redSlider.addEventListener("change", (event) => {
     bgColor.r = event.target.value;
+
     document.querySelector("span.r-value").textContent = bgColor.r;
     changeCircleColor();
-    changeHeading();
+    changeColors();
 });
 
 greenSlider.addEventListener("change", (event) => {
     bgColor.g = event.target.value;
+
     document.querySelector("span.g-value").textContent = bgColor.g;
     changeCircleColor();
-    changeHeading();
+    changeColors();
 });
 
 blueSlider.addEventListener("change", (event) => {
     bgColor.b = event.target.value;
+
     document.querySelector("span.b-value").textContent = bgColor.b;
     changeCircleColor();
-    changeHeading();
+    changeColors();
 });
+
 
 //Functions
 function changeCircleColor(){
@@ -54,11 +67,13 @@ function changeCircleColor(){
     circleDiv.style.backgroundColor = bgColor.toCSS(); 
 }
 
-
-function changeHeading(){
+function changeColors(){
     const headings = document.querySelectorAll("h1, h2,h3,h4,h6");
+
     for (let i = 0; i < headings.length; i++) {
         const h = headings[i];
         h.style.backgroundColor = bgColor.toHex();
     }
+
+    document.body.style.backgroundColor = bgColor.toHex();
 }
